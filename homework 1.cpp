2e1a20f4e78746b9ed265c1bf3cbe02e9478f06b
emberskyee <157657239+emberskyee@users.noreply.github.com>
2024-01-31 21:14:56 -0500

    Update homework 1.cpp

--- homework 1.cpp
+++ homework 1.cpp
@@ -1,4 +1,3 @@
-// Homework1Files.cpp : This file contains the 'main' function. Program execution begins and ends there.
 #include <iostream>
 #include <fstream>
 #include <sstream>
@@ -14,128 +13,116 @@ struct Book {
 	map<char, int> letterFrequency;
 	int lineCount;
 };
-/////
-
-Book readBook(ifstream& inputFile) {
-	Book currentBook;
-	string line;
-	string contents;
-	currentBook.wordCount = 0;
-	currentBook.lineCount = 0;
-
-	inputFile.open("MobyDick.txt");
-
-	if (!(inputFile.is_open())) {
-		cout << "File is not open!" << endl;
-		return currentBook;
-	}
-
-	// Read title
-	getline(inputFile, currentBook.title);
-
-	// Read author
-	getline(inputFile, currentBook.author);
-
-	// Contents line
-	getline(inputFile, contents);
-
-	// Count words
-	vector<string> words;
-	string word;
-
-	while (inputFile >> word) {
-		words.push_back(word);
-	}
-
-	for (const string& word : words) {
-		currentBook.wordCount++;
-	}
-
-	// Count letter frequency
-	for (const string& word : words) {
-		for (char letter : word) {
-			letter = tolower(letter);
-			if (isalpha(letter)) {
-				currentBook.letterFrequency[letter]++;
-			}
-		}
-	}
-
-	// Reset line reading back to the beginning for line count
-	inputFile.clear();
-	inputFile.seekg(0, ios::beg);
-
-	// Line count
-	while (getline(inputFile, line)) {
-		currentBook.lineCount++;
-	}
-
-	return currentBook;
-}
-////
-
-// write book info
-void writeToFile(const Book& book, std::ofstream& outputFile) {
-	outputFile << "Title: " << book.title << "\n";
-	outputFile << "Full Author: " << book.author << "\n";
-	std::istringstream authorStream(book.author);
-	std::string firstName, lastName;
-	authorStream >> firstName >> lastName;
-	outputFile << "Author First Name: " << firstName << "\n";
-	outputFile << "Author Last Name: " << lastName << "\n";
-	outputFile << "Word count: " << book.wordCount << "\n";
-	char showLetterFrequency;
-	cout << "Do you want to see the letter frequency for this book? (y/n): ";
-	cin >> showLetterFrequency;
-	if (showLetterFrequency == 'y' || showLetterFrequency == 'Y') {
-		outputFile << "Letter frequency:\n";
-		for (const auto& entry : book.letterFrequency) {
-			double percentage = static_cast<double>(entry.second) / book.wordCount * 100.0;
-			outputFile << entry.first << ": " << std::fixed << std::setprecision(2) << percentage << "%\n";
-		}
-	}
-
-	outputFile << "Line count: " << book.lineCount << "\n";
-	outputFile << "\n";
-
-	std::ofstream outFile("CardCatlog.txt", std::ios::app);
-	if (!outFile.is_open()) {
-		std::cerr << "Error opening output file." << std::endl;
-		return;
-	}
-	// write book info to file 
-	outFile << "Title: " << book.title << endl;
-}
 
 int main() {
-	string fileName;
-	cout << "Enter the name of the file to be processed: ";
-	cin >> fileName;
-
-	ifstream inputFile(fileName);
-
-	while (!(inputFile.is_open())) {
-		cerr << "Error opening the file. Please enter another file name: ";
+	char processAnother;
+	do {
+		Book bookInfo;
+		string fileName;
+		cout << "Enter the name of the file to be processed: ";
 		cin >> fileName;
-		inputFile.open(fileName);
-	}
-	ofstream outputFile("CardCatalog.txt", ios::app);
 
-	if (!outputFile.is_open()) {
-		cerr << "Error opening CardCatalog.txt for writing." << endl;
-		return 1;
-	}
+		ifstream inputFile(fileName);
 
-	while (inputFile.peek() != EOF) {
-		Book book = readBook(inputFile);
-		writeToFile(book, outputFile);
-	}
+		while (!(inputFile.is_open())) {
+			cerr << "Error opening the file. Please enter a valid file name: ";
+			cin >> fileName;
+			inputFile.open(fileName);
+		}
 
-	std::cout << "Card catalog information has been saved to CardCatalog.txt.\n";
+		string line;
+		string contents;
+		bookInfo.wordCount = 0;
+		bookInfo.lineCount = 0;
 
-	inputFile.close();
-	outputFile.close();
+		// Read title
+		getline(inputFile, bookInfo.title);
+
+		// Read author
+		getline(inputFile, bookInfo.author);
+
+		// Contents line to be ignored
+		getline(inputFile, contents);
+
+		// Count words
+		vector<string> words;
+		string word;
+
+		while (inputFile >> word) {
+			words.push_back(word);
+		}
+
+		for (const string& word : words) {
+			bookInfo.wordCount++;
+		}
+
+		// Count letter frequency
+		for (const string& word : words) {
+			for (char letter : word) {
+				letter = tolower(letter);
+				if (isalpha(letter)) {
+					bookInfo.letterFrequency[letter]++;
+				}
+			}
+		}
+
+		// Reset line reading back to the beginning for line count
+		inputFile.clear();
+		inputFile.seekg(0, ios::beg);
+
+		// Line count
+		while (getline(inputFile, line)) {
+			bookInfo.lineCount++;
+		}
+
+		// obtain the last line of the file that doesn't have a new line character at the end
+		if (inputFile.eof()) {
+			++bookInfo.lineCount;
+		}
+
+		// Prepare to write to file since book info has been read in
+		ofstream outputFile("CardCatalog.txt", ios::app);
+
+		if (!outputFile.is_open()) {
+			cerr << "Error opening CardCatalog.txt for writing." << endl;
+			return 1;
+		}
+
+		// Once file is opened/created successfully, begin writing book info
+		outputFile << "Title: " << bookInfo.title << "\n\n";
+		outputFile << "Full Author: " << bookInfo.author << "\n\n";
+
+		// Split the author's full name into first & last
+		istringstream authorStream(bookInfo.author);
+		string firstName, lastName;
+		authorStream >> firstName >> lastName;
+
+		outputFile << "Author First Name: " << firstName << "\n\n";
+		outputFile << "Author Last Name: " << lastName << "\n\n";
+		outputFile << "Word count: " << bookInfo.wordCount << "\n\n";
+		outputFile << "Line count: " << bookInfo.lineCount << "\n\n";
+
+		char showLetterFrequency;
+		cout << "Do you want to see the letter frequency for this book? (y/n): ";
+		cin >> showLetterFrequency;
+
+		if (showLetterFrequency == 'y' || showLetterFrequency == 'Y') {
+			cout << bookInfo.title << " letter frequency:\n";
+			for (const auto& entry : bookInfo.letterFrequency) {
+				double percentage = (static_cast<double>(entry.second) / bookInfo.wordCount) * 100.0;
+				cout << entry.first << ": " << fixed << setprecision(2) << percentage << "%\n";
+			}
+		}
+
+		cout << "Card catalog information has been saved to CardCatalog.txt.\n";
+
+		inputFile.close();
+		outputFile.close();
+
+		cout << "Would you like to process another book? (y/n)\n";
+		cin >> processAnother;
+
+	} while (processAnother == 'y' || processAnother == 'Y');
 
 	return 0;
-}			
-	
+}
